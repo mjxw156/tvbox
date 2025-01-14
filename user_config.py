cfcdca7f2813150743c8652f634224fc0e40d188
mjxw156 <1562092940@qq.com>
2025-01-14 22:06:23 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,14 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"	
+favorite_list =[	
+     “广东珠江”，	
+     CCTV-1",	
+     CCTV-5"	
+]
+favorite_page_num =6	
+default_page_num =4	
+urls_limit = 15	
+response_time_weight = 0.5	
+resolution weight =0.5
+recent_days =20
+ipv_type = "ipv4"
